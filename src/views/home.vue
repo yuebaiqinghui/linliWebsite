4975b9cfc4121bccfd90d0a2201d9ee5756de295
徐佳枫 <604039372@qq.com>
2019-09-25 10:29:41 +0800

    项目初始化完成

---
+++ src/views/home.vue
@@ -0,0 +1,14 @@
+<template>
+<div class="layout">
+    <Layout>
+        <Header>Header</Header>
+        <Content>Content</Content>
+        <Footer>Footer</Footer>
+    </Layout>
+</div>
+</template>
+<script>
+export default {
+
+}
+</script>
